bc55177f9d151f531fe4bfc549b350b2ebe3a18e
danuseptiadi <danuseptiadi13@gmail.com>
2022-10-17 22:41:55 +0700

    CRUD

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-document.getElementById("add").onclick=() =>{
+let arr = [];
+function singkron() {    
+    document.getElementById("lists").innerHTML = "";
+    for (let i = 0; i < arr.length; i++) {
+    document.getElementById("lists").innerHTML += `<li id="${i}">${arr[i]}
+    <input type="text" id="edit${i}" />
+    <button onclick="edit(${i});">Edit</button><button onclick="del(${i});">delete</button></li>`;
+    
+}}
+
+
+function add() {
     let value = document.getElementById("task").value;
-    document.getElementById("lists").innerHTML += `<li id="${value}">${value}</li>`;
+    arr.push(value);
+    singkron();
+}
+
+function edit(i) {
+    arr[i] = document.getElementById(`edit${i}`).value;
+    singkron();
+}
+
+function del(i) {
+    arr.splice(i, 1);
+    singkron();
 }
